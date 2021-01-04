9ccac4a716343724a453e14b631521eed1200ff9
li <@li:maisiliym.uniks>
2021-01-04 08:35:05 +0000

    refine nix de/recompose idea

--- mkSubModulesSrc.nix
+++ mkSubModulesSrc.nix
@@ -1,13 +1,39 @@
-mkSubmodulesSrc {
-  root = rootGitRepoFlake.self;
+let
+  recomposedInputAkaGitSubmodules = mkSubmodulesSrc {
+    root = rootGitRepoFlake;
 
-  submodules = {
-    "submodule/insubdir/repo" = repoFlake.self;
+    submodules = {
+      "submodule/insubdir/repo" = repoFlake;
 
-    systemd = fetchGit {
-      url = file:///git/github.com/systemd/systemd;
-      rev = "ae366f3acbc1a45504e9875099b17a7e1a221d03";
+      systemd = fetchGit {
+        url = file:///git/github.com/systemd/systemd;
+        rev = "ae366f3acbc1a45504e9875099b17a7e1a221d03";
+      };
     };
+  };
 
+  decomposedInputAkaRustCratesFromGit = decomposeSrc {
+    src = someFlake;
+    outputs = {
+      outputNameFoo = [ "path/fileToKeep.cpp" "makefile" ];
+      outputNameBar = [ "src/whatever.rs" "build.rs" ];
+    };
+  };
+
+in
+{
+  rustProject = mkDerivation {
+    # ...
+    src = decomposedInputAkaRustCratesFromGit.outputNameBar;
+  };
+
+  cppProject = mkDerivation {
+    # ...
+    src = decomposedInputAkaRustCratesFromGit.outputNameFoo;
+  };
+
+  submodulesProject = mkDerivation {
+    # ...
+    src = recomposedInputAkaGitSubmodules;
   };
 }

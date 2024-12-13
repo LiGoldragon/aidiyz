d688ccefc3a0bb363e993ebef62370bd27bc6b79
li <li@maisiliym.criome.me>
2024-12-13 14:10:52 +0100

    scaffold atom

--- flake.nix
+++ flake.nix
@@ -1,8 +1,29 @@
 {
-  description = "aidiyz";
+  description = "Aidiyz (Ideas)";
 
-  outputs = { self }: {
-    datom = {};
-
+  inputs = {
+    atom.url = "github:LiGoldragon/atom";
+    lib.url = "github:ekala-project/nix-lib";
   };
+
+  outputs = inputs@{ self, ... }:
+    let
+      std = inputs.lib.lib // builtins;
+
+      Atom = inputs.atom.mod;
+      inherit (Atom) importAtom;
+
+      exportJSON = name: data: std.toFile (name + ".json") (std.toJSON data);
+
+    in
+    {
+      atom = {
+        introspection.simple = exportJSON "atom-module" (std.attrNames Atom);
+        introspection.trace = exportJSON "atom-module" (std.trace (Atom) null);
+
+        simple = importAtom { } (./. + "atom/simple/simple@.toml");
+
+      };
+
+    };
 }

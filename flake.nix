436becd484136ad1cbb49b721732cda858b41d19
li <li@maisiliym.criome.me>
2025-01-03 20:49:26 +0100

    added(flake.introspection)

--- flake.nix
+++ flake.nix
@@ -1,12 +1,37 @@
 {
   description = "Aidiyz (Ideas)";
 
-  inputs.atom.url = "github:LiGoldragon/atom";
+  inputs = {
+    atom.url = "github:LiGoldragon/atom";
+    lib.url = "github:nix-community/nixpkgs.lib";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
 
   outputs =
-    { atom, ... }:
-    atom.mkAtomicFlake {
-      manifest = ./. + "/atom/simple@.toml";
-      noSystemManifest = ./. + "/atom/local-lib@.toml";
+    {
+      self,
+      lib,
+      atom,
+      ...
+    }@inputs:
+    let
+      inherit (inputs.lib) lib;
+
+      atomOutputs = atom.mkAtomicFlake {
+        manifest = ./. + "/atom/simple@.toml";
+        noSystemManifest = ./. + "/atom/local-lib@.toml";
+      };
+
+    in
+    atomOutputs
+    // {
+      introspection = {
+        self = lib.traceSeqN 2 (builtins.removeAttrs self [ "introspection" ]) true;
+        outputs = lib.traceSeqN 3 (builtins.removeAttrs self.outputs [ "introspection" ]) true;
+        inputs = lib.traceSeqN 2 self.inputs true;
+      };
     };
 }
